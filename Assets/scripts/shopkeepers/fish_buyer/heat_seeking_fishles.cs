0e9ce3ff948024042fa72204f87b6e2bf22a62b3
kevynthefox <carmodykevyn6@gmail.com>
2026-02-17 08:59:30 -0500

    got ranged fish to shoot the player again.
    the issue was that i didn't set their "what is player" value.
    
    also pussy don keeps krilling himself

--- Assets/scripts/shopkeepers/fish_buyer/heat_seeking_fishles.cs
+++ Assets/scripts/shopkeepers/fish_buyer/heat_seeking_fishles.cs
@@ -55,6 +55,13 @@ public class heat_seeking_fishles : MonoBehaviour
 
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange);
+    }
     void Update()
     {
         if (TimeManager.current.update == true)
