a536ec78df931d68efa3f8ace37e8d52685c96ad
kevynthefox <carmodykevyn6@gmail.com>
2026-03-24 09:04:43 -0400

    replaced the tutorial im using for a_star pathfinding
    https://www.youtube.com/watch?v=gNmPmWR2vV4

---
+++ Assets/scripts/A_Star_Pathfinding/octree_method/Octree.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+//code from this tutorial: https://www.youtube.com/watch?v=gNmPmWR2vV4
+namespace Octrees
+{
+    public class Octree
+    {
+        public OctreeNode root;
+        Bounds bounds;
+
+        public Octree(GameObject[] worldObjects, float minNodeSize)
+        {
+        }
+    }
+    
+    public class OctreeNode
+    {
+       
+    }
+    public class OctreeObject
+    {
+       
+    }
+}
